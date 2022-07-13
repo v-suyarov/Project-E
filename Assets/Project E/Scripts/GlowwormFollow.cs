33fafa613304b8c784dbc2cd060ec4be2fb48182
v-suyarov <90338171+v-suyarov@users.noreply.github.com>
2022-07-14 01:47:34 +0300

    Fix mechanic

--- Assets/Project E/Scripts/GlowwormFollow.cs
+++ Assets/Project E/Scripts/GlowwormFollow.cs
@@ -6,122 +6,190 @@ using System;
 
 namespace MoreMountains.CorgiEngine
 {
+
+    
+   
     public class GlowwormFollow : MonoBehaviour
     {
-
-        public CorgiController controller;
-        public Transform DefaultPosition;
-        public float offset_x = 2;
-        public float offset_y = 2;
-        public float period = 2;
-
-        private float max_speed = 15;
-        public float speed = 10f;
-        //dsada
-        public float error = 0.1f;
-
-        private Vector3 def;
-        private Vector3 light;
-        // Start is called before the first frame update
-        void Start()
+        public enum FlightBehavior
         {
-            Vector3 graf = Vector3.zero;
-
-            for (float x = 0; x < 8; x += 0.1f)
-            {
-                graf.x = x;
-                graf.y = (float)(Math.Sin(x * Math.PI / 8) + 1.5f);
-                
-                Debug.DrawLine(graf, graf+Vector3.down, Color.red, 10f);
-
-            }
-
-
+           Sin,
+           Cos,
+           Negative_sin,
+           Negative_cos,
+           
+           Castom_function_1,
+                    
         }
+        [Header("Flight Behavior")]
+        [Tooltip("Насколько далеко cветлячок будет отлетать от персонажа")]
+        [SerializeField] private float distanse = 4f;
+        [Tooltip("Выбор функции влияет на траекторию полета")]
+        [SerializeField] private FlightBehavior flightBehavior;
+        [Tooltip("Переод функции")]
+        [SerializeField] float  period = 2;
+        [Tooltip("Умножает функцию на указанное значение")]
+        [SerializeField] private float multiply =1f;
+        [Tooltip("Скорость полета")]
+        [SerializeField] private float speed;
+        //[Tooltip("Сдвиг светлячка в состоянии покоя")]
+        private float offset = 0.1f;
+        private GameObject player;       
+        private float elapsedTime = 0f;
+        private float progress =  0.5f;
+        private float timeRest = 0.0f;
+        private Vector2 defaultLocalPos= Vector2.zero;
+        //ошабка при сравнении цифр с плавающей точкой
+        private float error = 1f;
 
-        // Update is called once per frame
-        void Update()
+
+        private void Start()
+        {   //запоминаю дефолтную позицию
+            defaultLocalPos = transform.localPosition;
+            player = transform.root.gameObject;
+            //для более удобной настройки
+           
+        }
+        private void Update()
         {
-            float deviation = controller.Speed.x / max_speed;
 
-            Vector3 glowworm_local_pos = controller.transform.InverseTransformPoint(transform.position);
-            Vector3 default_local_pos = controller.transform.InverseTransformPoint(DefaultPosition.position);
-            if (Math.Abs(deviation) > 1)
-                deviation = deviation / Mathf.Abs(deviation);
-
-            if (Mathf.Abs(deviation) <= 0.01)
+            Debug.Log(player.GetComponent<Character>().IsFacingRight);
+            if (player.GetComponent<CorgiController>().Speed.x>error&&progress<1)
             {
-                if (Mathf.Abs(glowworm_local_pos.x) >= error)
-                {
-                    Vector3 next_position = Vector3.zero;
-                 
-                    next_position.y = (float)( Math.Sin(glowworm_local_pos.x * Math.PI / 8+glowworm_local_pos.x) + 1.5f);
-
-                    Vector3 direction = default_local_pos - glowworm_local_pos;
-                    direction.y = next_position.y - glowworm_local_pos.y;
                    
+                    progress += Time.deltaTime / 8 * speed;
+                    Vector2 nextPos = Vector2.Lerp(new Vector2(-distanse, 0), new Vector2(distanse, 0), progress);
+                    nextPos.y = GetY_ByFunction(nextPos.x, flightBehavior);
+                    Debug.DrawLine(transform.position, transform.position + new Vector3(0.1f, 0, 0), Color.red, 5f);
+                    transform.localPosition = nextPos;
+                    if (progress > 1)
+                    {
+                        progress = 1;
+                        
+                    }
+                
+            }
+            if(player.GetComponent<CorgiController>().Speed.x < -error && progress > 0)
+            {
 
-                    if (Mathf.Abs(glowworm_local_pos.x) <= 0.01)
+                    progress -= Time.deltaTime / 8 * speed;
+                    Vector2 nextPos = Vector2.Lerp(new Vector2(-distanse, 0), new Vector2(distanse, 0), progress);
+                    nextPos.y = GetY_ByFunction(nextPos.x, flightBehavior);
+                    Debug.DrawLine(transform.position, transform.position + new Vector3(0.1f, 0, 0), Color.red, 5f);
+                    transform.localPosition = nextPos;
+                    if (progress < 0)
+                    {
+                        progress = 0;
+
+                    }              
+            }
+            if ( Math.Abs( player.GetComponent<CorgiController>().Speed.x) <= error  )
+            {
+                if (progress <0.5f-offset||progress>0.5f+offset)
+                {
+                    float prev_progress = progress;
+                    if (progress > 0.5)
                     {
 
+                        progress -= Time.deltaTime / 8 * speed;
+                        if (player.GetComponent<Character>().IsFacingRight && progress < 0.5f + offset)
+                        {
+                            progress = prev_progress;
+                        }
                     }
+                    else
+                    {
+                        progress += Time.deltaTime / 8 * speed;
+                        if (!player.GetComponent<Character>().IsFacingRight && progress > 0.5f - offset)
+                        {
+                            progress = prev_progress;
+                        }
 
-                    //transform.Translate(direction.normalized * speed * Time.deltaTime, Space.Self);
-                    
-                    transform.position = Vector2.Lerp(transform.position, DefaultPosition.position, Time.deltaTime);
+                    }
+                    Vector2 nextPos = Vector2.Lerp(new Vector2(-distanse, 0), new Vector2(distanse, 0), progress);
+                    nextPos.y = GetY_ByFunction(nextPos.x, flightBehavior);
 
+                    Debug.DrawLine(transform.position, transform.position + new Vector3(0.1f, 0, 0), Color.red, 5f);
+                    transform.localPosition = nextPos;
+                    if (Math.Abs(progress - 0.5) < 0.02)
+                    {
+                        progress = 0.5f;
 
-
-                    Vector3 temp = transform.position;
-                    
-                    Vector3 temp1 = temp;
-                    temp1.x += 0.1f;
-                    Debug.DrawLine(temp, temp1, Color.red, 10f);
-
-
-                    //Debug.Log(pos_y.y.ToString()+": Y");
+                    }
                 }
-            }
-            else if (deviation > 0.01)
-            {
-                if (Mathf.Abs(glowworm_local_pos.x - offset_x) >= error)
+                else
                 {
-                    Vector3 right_defaultPosition = DefaultPosition.position;
-                    right_defaultPosition.x += offset_x;
+                    if (player.GetComponent<Character>().IsFacingRight)
+                    {
+                        progress += Time.deltaTime / 8 * speed;
+                    }
+                    else
+                    {
+                        progress -= Time.deltaTime / 8 * speed;
+                    }
+                    Vector2 nextPos = Vector2.Lerp(new Vector2(-distanse, 0), new Vector2(distanse, 0), progress);
+                    nextPos.y = GetY_ByFunction(nextPos.x, flightBehavior);
 
-                    transform.Translate((right_defaultPosition - transform.position).normalized * speed * Time.deltaTime * Mathf.Abs(deviation), Space.World);
+                    
+                    transform.localPosition = nextPos;
                 }
             }
-            else if (deviation < 0.01)
+            if (player.GetComponent<Character>().MovementState.CurrentState==CharacterStates.MovementStates.Idle)
             {
-                if (Mathf.Abs(glowworm_local_pos.x + offset_x) >= error)
-                {
 
-                    Vector3 right_defaultPosition = DefaultPosition.position;
-                    right_defaultPosition.x -= offset_x;
-
-                    transform.Translate((right_defaultPosition - transform.position).normalized * speed * Time.deltaTime * Mathf.Abs(deviation), Space.World);
-                }
             }
 
-            def = DefaultPosition.position;
-            light = transform.position;
-            // Debug.Log(();
-
-            
+           
         }
+
+        public float GetY_ByFunction(float x,FlightBehavior function)
+        { 
+            x = Mathf.Abs(x);
+            if(function==FlightBehavior.Sin)
+            {
+                return ((float)Math.Sin(x * Math.PI / period) *multiply)+defaultLocalPos.y;
+            }
+            else if(function == FlightBehavior.Cos)
+            {
+
+                return ((float)Math.Cos(x * Math.PI / period) * multiply) + defaultLocalPos.y;
+
+            }
+            else if (function == FlightBehavior.Negative_sin)
+            {
+
+                return -((float)Math.Sin(x * Math.PI / period) * multiply) + defaultLocalPos.y;
+
+            }
+            else if (function == FlightBehavior.Negative_cos)
+            {
+
+                return -((float)Math.Cos(x * Math.PI / period) * multiply) + defaultLocalPos.y+1*multiply;
+
+            }
+            else if (function == FlightBehavior.Castom_function_1)
+            {
+                //в итоге позволит завtршить движение на y=0, работает только при периоде = 1
+                float normolize = 4.2549f / distanse;
+                return ((float)Math.Sin(x*normolize * Math.PI / period - 7) * x*normolize / 10 * multiply) + defaultLocalPos.y;
+
+            }
+
+            return defaultLocalPos.y;
+        }
+
     }
     
-}
+
     public class Gizmo : MonoBehaviour
     {
-   
-    void OnDrawGizmos() //Встроенный метод,необходим для работы с Gizmos объектами и их отрисовки
-    {
-        Gizmos.color = Color.green;
 
-        
-        Gizmos.DrawRay(transform.position,Vector3.right);
+        void OnDrawGizmos() //Встроенный метод,необходим для работы с Gizmos объектами и их отрисовки
+        {
+            Gizmos.color = Color.green;
+
+
+            Gizmos.DrawRay(transform.position, Vector3.right);
+        }
     }
-
 }
